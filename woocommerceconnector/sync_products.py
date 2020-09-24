6dd961ad910028945bb935f6163af70a23c98bb3
june23rd1987 <june23rd1987@gmail.com>
2020-09-24 12:53:49 +0800

    Added Optional sync of categories

--- woocommerceconnector/sync_products.py
+++ woocommerceconnector/sync_products.py
@@ -180,17 +180,29 @@ def sync_item_with_woocommerce(item, price_list, warehouse, woocommerce_item=Non
     variant_list = []
     wc_product_category_id = frappe.db.get_value(
         "Item Group", item.item_group, "woocommerce_id_za") #jupiter - additional
-    item_data = {
-            "name": item.get("item_name"),
-            "description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
-            "short_description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
-            "sku": item.get("item_code"),                                                                                                #jupiter - additional
-            "categories": [
-                {
-                    "id": wc_product_category_id                                                                                        #jupiter - additional
-                }
-            ],                                                      
-    }
+    
+    woocommerce_settings = frappe.get_doc("WooCommerce Config", "WooCommerce Config")                                                       #jupiter additional
+    if woocommerce_settings.sync_itemgroup_to_wp_categories:                                                                                #optional syncing of categories
+        item_data = {
+                "name": item.get("item_name"),
+                "description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
+                "short_description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
+                "sku": item.get("item_code"),                                                                                                #jupiter - additional
+                "categories": [
+                    {
+                        "id": wc_product_category_id                                                                                        #jupiter - additional
+                    }
+                ],                                                      
+        }
+    else:
+        item_data = {
+                "name": item.get("item_name"),
+                "description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
+                "short_description": item.get("woocommerce_description") or item.get("web_long_description") or item.get("description"),
+                "sku": item.get("item_code"),                                                                                                #jupiter - additional                                                     
+        }
+        
+        
     item_data.update( get_price_and_stock_details(item, warehouse, price_list) )
 
     if item.get("has_variants"):  # we are dealing a variable product
